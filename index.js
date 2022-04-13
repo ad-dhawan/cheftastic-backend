bc5c49b3c70edde5f0d6923f8f70d54e6a06d681
Ankit Dhawan <ankit.ad.dhawan@gmail.com>
2022-04-13 11:34:51 +0530

    [ADDED] Firebase SDK

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const postRoute = require("./src/routes/post");
 //CONFIGURATION
 dotenv.config();
 
+admin.initializeApp({
+  credential: admin.credential.cert({
+    "type": process.env.FIREBASE_TYPE,
+    "project_id": process.env.FIREBASE_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_AUTH_URI,
+    "token_uri": process.env.FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER,
+    "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT
+  }),
+});
+
 //DATABASE CONNECT
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
